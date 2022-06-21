fd975e55da860c750b4673c8b3495505971c6ce8
HuseynJafarov <huseynnj@code.edu.az>
2022-06-21 16:38:33 +0400

    NewHomeWorkC#StringMethods

---
+++ reverse writing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace reverse_writing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            StringReverse("KeleM");
+
+
+
+        }
+
+        //Reverse String
+        static void StringReverse(string name)
+        {
+            string Reverse = String.Empty;
+
+            for (int i = name.Length - 1; i > -1; i--)
+            {
+                Reverse += name[i];
+            }
+
+            Console.WriteLine(Reverse);
+
+
+        }
+            
+
+
+    }
+
+}
